9aebe007b80ed2098513ce836721c08a5ba559fd
Dmytro Tsymbal <dmytro.tsymbal@stuzo.com>
2014-05-02 16:33:09 +0300

    pushplugin

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,28 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+//        for (var key in device) {
+//            alert('device[' + key + '] = ' + device[key]);
+//        }
         app.receivedEvent('deviceready');
+
+        if ( device.platform == 'android' || device.platform == 'Android' ) {
+            pushNotification.register(
+                app.successHandler,
+                app.errorHandler, {
+                    "senderID":"939790234612",
+                    "ecb":"onNotificationGCM"
+                });
+        } else {
+            pushNotification.register(
+                tokenHandler,
+                errorHandler, {
+                    "badge":"true",
+                    "sound":"true",
+                    "alert":"true",
+                    "ecb":"onNotificationAPN"
+                });
+        }
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +66,66 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    successHandler: function() {
+        alert('result = ' + result);
+    },
+    errorHandler: function() {
+        alert('error = ' + error);
     }
 };
+
+function onNotificationGCM(e) {
+
+    alert('onNotificationGCM');
+    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                // Your GCM push server needs to know the regID before it can push to this device
+                // here is where you might want to send it the regID for later use.
+                console.log("regID = " + e.regid);
+            }
+            break;
+
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if ( e.foreground )
+            {
+                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+                // if the notification contains a soundname, play it.
+                var my_media = new Media("/android_asset/www/"+e.soundname);
+                my_media.play();
+            }
+            else
+            {  // otherwise we were launched because the user touched a notification in the notification tray.
+                if ( e.coldstart )
+                {
+                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                }
+                else
+                {
+                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                }
+            }
+
+            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+            break;
+
+        case 'error':
+            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+            break;
+
+        default:
+            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+            break;
+    }
+}
